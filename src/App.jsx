58e17a2ac79cf7ee0578e87d2b85b4355f605ff8
pavan <pavanvenkatanagamanoj@gmail.com>
2025-03-10 06:31:22 +0530

    implemented the movies preview"

--- src/App.jsx
+++ src/App.jsx
@@ -12,26 +12,32 @@ const API_OPTIONS={
 const App=()=>{
   const [searchTerm,setSearchTerm]=useState('');
   const [errorMessage,setErrorMessage]=useState('');
-  const[MovieList,setMovieList]=useState('');
-  const fetchMovies=async ()=>{
+  const [movieList,setMovieList]=useState([]);
+  
+  const fetchMovies=async ()=> {
     try {
       const response=await fetch(API_BASE_URL,API_OPTIONS)
       if(!response.ok){
         throw new Error("Failed to fetch movies")
       }
+      
       const data=await response.json()
-      if(data.Response ==='False'){
-        setErrorMessage(data.error||"Failed to fetch movies");
+      
+      if(data.results){
+        setMovieList(data.results);
+      } else {
+        setErrorMessage("Failed to fetch movies");
         setMovieList([]);
-        return;
       }
     } catch (error) {
-      setErrorMessage(`Error while fetching movies${error}`);
+      setErrorMessage(`Error while fetching movies: ${error.message}`);
     }
   }
+  
   useEffect(()=>{
     fetchMovies()
   },[])
+  
   return(
     <main>
       <div className="pattern"/>
@@ -39,13 +45,34 @@ const App=()=>{
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Find <span className="text-gradient">Movies</span> Without The Hassle</h1>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
         <section className="all-movies">
           <h2>All Movies</h2>
+          <ul>
+            {movieList && movieList.length > 0 ? (
+              movieList.map((movie) => (
+                <li key={movie.id} className="movie-card">
+                  <img 
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                    alt={movie.title} 
+                  />
+                  <div className="content">
+                    <h3>{movie.original_title}</h3>
+                    <div className="rating">
+                      <img src="/star.svg" alt="rating" />
+                      <p>{movie.vote_average.toFixed(1)}</p>
+                    </div>
+                    <span className="year">{movie.release_date?.split('-')[0]}</span>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <p>No movies found</p>
+            )}
+          </ul>
           {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         </section>
-        <h1 className="text-white">{searchTerm}</h1>
       </div>
     </main>
   )
