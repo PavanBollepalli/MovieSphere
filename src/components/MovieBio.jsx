6ce5de91ec2e86d692be7e5051513142d0db0bb2
pavan <pavanvenkatanagamanoj@gmail.com>
2025-03-17 06:07:35 +0530

    trying to implement the figma design for movie bio page

--- src/components/MovieBio.jsx
+++ src/components/MovieBio.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-
 const MovieBio = () => {
   const [movieDetails, setMovieDetails] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -39,55 +38,23 @@ const MovieBio = () => {
   if (!movieDetails) return <div>No movie details available</div>;
 
   return (
-    <div className="text-white m-4 font-mono movie-card">
-      <h2>{movieDetails.title}</h2>
-
-      <div className="flex text-white align-center justify-center ">
-        {movieDetails.poster_path && (
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
-            alt={movieDetails.title}
-            className="movie-poster  w-128 h-128"
-          />
-        )}
-
-        <div className=" m-2 p-4 font-mono text-lg">
-          {movieDetails.tagline && (
-            <p className="tagline text-xl text-bold text-orange-200">
-              Tag Line:
-              {movieDetails.tagline}
-            </p>
-          )}
-          <p className="overview">{movieDetails.overview}</p>
-
-          <div className="meta">
+    <div className="font-sans m-2 p-4 text-white movie-card shadow-md shadow-white">
+      <section className="flex items-center justify-between">
+        <div>
+          <h1>{movieDetails.title}</h1>
+          <div className="flex items-start gap-2">
             <p>
-              <strong>Rating:</strong> {movieDetails.vote_average?.toFixed(1)}
-              /10
+              {movieDetails.release_date
+                ? movieDetails.release_date.split("-")[0]
+                : "N/A"}
             </p>
-            <p>
-              <strong>Release Date:</strong> {movieDetails.release_date}
-            </p>
-            {movieDetails.runtime && (
-              <p>
-                <strong>Runtime:</strong> {movieDetails.runtime} minutes
-              </p>
-            )}
-            {movieDetails.genres && (
-              <p>
-                <strong>Genres:</strong>{" "}
-                {movieDetails.genres.map((g) => g.name).join(", ")}
-              </p>
-            )}
-            <Link
-              to="/"
-              className="p-1 mt-4 bg-violet-200 text-center text-violet-500"
-            >
-              <button>Go Back To Home</button>
-            </Link>
+            <p>·</p>
+            <p>{movieDetails.original_language}</p>
           </div>
         </div>
-      </div>
+      </section>
+      <section></section>
+      <section></section>
     </div>
   );
 };
